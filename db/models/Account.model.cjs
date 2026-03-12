4ef2d7dfd26ddba5f758d860caf5a8575198cd3c
Daniel Gonzalez <dboyg123.dg@gmail.com>
2026-03-12 08:15:47 -0400

    removed the unnecessary "name" field in Accounts model and seed

--- db/models/Account.model.cjs
+++ db/models/Account.model.cjs
@@ -4,8 +4,7 @@ const ACCOUNT_TYPES = ['Debit', 'Credit'];
 const ACCOUNT_CATEGORIES = ['Checking', 'Credit', 'Savings', 'Investments', 'Loan'];
 
 const accountSchema = new mongoose.Schema({
-    name: { type: String, required: true },           // e.g. "Amex Credit"
-    displayName: { type: String },                     // e.g. "Amex Credit Card"
+    displayName: { type: String, required: true },                     // e.g. "Amex Credit Card"
     institution: { type: String, required: true },     // e.g. "Amex", "SoFi", "Navy Fed"
     type: { type: String, enum: ACCOUNT_TYPES, required: true },
     category: { type: String, enum: ACCOUNT_CATEGORIES, required: true },
